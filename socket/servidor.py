80203d2251b57dcd0ffd68cd4af4b9df42a3ffd9
catarinaUni <catarina.sampaio@discente.univasf.edu.br>
2024-09-09 12:21:56 +0000

    upppp

--- socket/servidor.py
+++ socket/servidor.py
@@ -1,21 +1,21 @@
 import socket
-def server(host = 'localhost', port=8082):
+def server(host = '0.0.0.0', port=8082):
     data_payload = 2048
     sock = socket.socket(socket.AF_INET,  socket.SOCK_STREAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_address = (host, port)
-    print ("Starting up echo server  on %s port %s" % server_address)
+    print ("Servidor iniciado em: %s porta %s" % server_address)
     sock.bind(server_address)
     sock.listen(5) 
     i = 0
     while True: 
-        print ("Waiting to receive message from client")
+        print ("Esperando comunicação do cliente...")
         client, address = sock.accept() 
         data = client.recv(data_payload) 
         if data:
-            print ("Data: %s" %data)
+            print ("Mensagem recebida: %s" %data)
             client.send(data)
-            print ("sent %s bytes back to %s" % (data, address))
+            print ("Enviando %s de volta para %s" % (data, address))
             client.close()
             i+=1
             if i>=3: break           
