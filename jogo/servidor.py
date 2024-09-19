50b875ff5011bcd0691c41342d8516c8a21b6878
catarinaUni <catarina.sampaio@discente.univasf.edu.br>
2024-09-19 19:48:36 -0300

    Update servidor.py

--- jogo/servidor.py
+++ jogo/servidor.py
@@ -1,89 +1,73 @@
 import socket
-import threading
 import random
-import time
 
-def handle_client(client_socket):
-    global current_turn
-    while True:
-        try:
-            command = client_socket.recv(1024).decode('utf-8').strip()
-            if not command:
-                break
+def exibir_tabuleiro(placar):
+    tabuleiro = ['_'] * 10
+    s_pos = placar[1] if placar[1] < 10 else 9
+    c_pos = placar[0] if placar[0] < 10 else 9
 
-            if command == 'roll':
-                if current_turn == 0:
-                    dice_roll = random.randint(1, 6)
-                    positions[0] += dice_roll
-                    response = f"Cliente rolou um {dice_roll} e avançou para a posição {positions[0]}\n"
-                    client_socket.send(response.encode('utf-8'))
+    if s_pos == c_pos:
+        tabuleiro[s_pos] = 'S/C'
+    else:
+        tabuleiro[s_pos] = 'S'
+        tabuleiro[c_pos] = 'C'
 
-                    # Verificar se há um vencedor
-                    if positions[0] >= 20:
-                        client_socket.send("Você ganhou! O jogo acabou.".encode('utf-8'))
-                        break
+    return ''.join(tabuleiro) + "\n"
 
-                    # Alternar turno
-                    current_turn = 1
-                    client_socket.send("Aguardando a sua vez.".encode('utf-8'))
-                else:
-                    client_socket.send("Aguarde sua vez.".encode('utf-8'))
-                    
-            else:
-                client_socket.send("Comando inválido. Use 'roll' para rolar o dado.".encode('utf-8'))
 
-        except ConnectionResetError:
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(('0.0.0.0', 8080))
+server.listen(1)
+
+client_socket, addr = server.accept()
+placar = [0, 0]
+inicioJogo = exibir_tabuleiro(placar)
+
+client_socket.send("---- INICIO DO JOGO ---- \n\n".encode('utf-8') + inicioJogo.encode('utf-8'))
+
+print("---- INICIO DO JOGO ---- \n\n")
+print(inicioJogo)
+
+while True:
+    client_socket.send("\n[C] Digite 1 para rolar o dado:  ".encode('utf-8'))
+    message = client_socket.recv(1024).decode('utf-8').strip()
+    if message == "1":
+        dado = random.randint(1, 6)
+        placar[0] += dado
+        if placar[0] >= 10:
+            tabuleiro = exibir_tabuleiro(placar)
+            client_socket.send(tabuleiro.encode('utf-8'))
+            print(tabuleiro)
+            mensagemVencedor = f"O Cliente obteve {dado} e chegou à linha de chegada!!!"
+            client_socket.send(mensagemVencedor.encode('utf-8'))
+            print(mensagemVencedor)
             break
+        else:
+            mensagemDado = f"O Cliente jogou o dado e obteve: {dado}\n"
+            client_socket.send(mensagemDado.encode('utf-8'))
+            print(mensagemDado)
 
-    client_socket.close()
-
-def handle_server_turn(client_socket):
-    global current_turn, positions
-    while True:
-        time.sleep(1)  # wait for 1 second
-        if current_turn == 1:
-            while True:
-                command = input("Digite o comando (roll): ")
-                if command == 'roll':
-                    server_dice_roll = random.randint(1, 6)
-                    positions[1] += server_dice_roll
-                    response = f"Servidor rolou um {server_dice_roll} e avançou para a posição {positions[1]}\n"
-                    client_socket.send(response.encode('utf-8'))
+            tabuleiro = exibir_tabuleiro(placar)
+            client_socket.send(tabuleiro.encode('utf-8'))
+            print(tabuleiro)
+        
+            resposta = input("[S] Digite 1 para rolar o dado:  ")
+            if resposta == "1":
+                dado = random.randint(1, 6)
+                placar[1] += dado
+                if placar[1] >= 10:
+                    tabuleiro = exibir_tabuleiro(placar)
+                    client_socket.send(tabuleiro.encode('utf-8'))
+                    print(tabuleiro)
+                    mensagemVencedor = f"O Servidor obteve {dado} e chegou à linha de chegada!!!"
+                    client_socket.send(mensagemVencedor.encode('utf-8'))
+                    print(mensagemVencedor)
                     break
                 else:
-                    print("Comando inválido. Use 'roll' para rolar o dado.")
-                    
-            if positions[1] >= 20:
-                client_socket.send("O servidor ganhou! O jogo acabou.".encode('utf-8'))
-                break
+                    mensagemDado = f"O Servidor jogou o dado e obteve: {dado}\n"
+                    client_socket.send(mensagemDado.encode('utf-8'))
+                    print(mensagemDado)
 
-            # Alternar turno
-            current_turn = 0
-            client_socket.send("Aguardando a sua vez.".encode('utf-8'))
-
-def main():
-    global current_turn, positions
-    current_turn = 0
-    positions = [0, 0] 
-
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(('0.0.0.0', 5558))
-    server.listen(1)
-    print("Servidor aguardando conexão...")
-
-    try:
-        client_socket, addr = server.accept()
-        print(f"Cliente conectado de {addr}")
-
-        client_socket.send("Bem-vindo ao jogo! Você é o Cliente.".encode('utf-8'))
-
-        client_thread = threading.Thread(target=handle_client, args=(client_socket,))
-        client_thread.start()
-
-        server_thread = threading.Thread(target=handle_server_turn, args=(client_socket,))
-        server_thread.start()
-    finally:
-        server.close()  # Certifique-se de fechar o socket do servidor
-
-if __name__ == "__main__":
-    main()
+                    tabuleiro = exibir_tabuleiro(placar)
+                    client_socket.send(tabuleiro.encode('utf-8'))
+                    print(tabuleiro)
